2071c92bfc728a334a348f81f13c4986fcff4bcb
Abeeb <abeebdon@gmail.com>
2024-02-18 01:46:30 +0100

    jst

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -13,7 +13,9 @@ const Home = () => {
         </p>
         <div className="github-link">
           <a href="https://github.com/abeeebdon">GITHUB</a>
-          <a href="https://docs.google.com/document/d/1uAtKn9vDBwG4MTe3zUM-WjAaE10SCAE4b04Gt8fDrPY/edit?usp=sharing">MY CV</a>
+          <a href="https://docs.google.com/document/d/13JHMJn6lYmiokmuwLlU12pZQT1rPRrdtH9HT_n7w6c8/edit?usp=drivesdk">
+            MY CV
+          </a>
         </div>
       </div>
       <div className="image">
