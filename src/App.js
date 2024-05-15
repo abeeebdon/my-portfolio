4a3cf0358dc1016140f0fe2c60773accaa1c466f
abeebdon <abeebdon@gmail.com>
2024-05-15 15:02:24 +0100

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
+
 import Header from './Header'
 import Home from './Home'
-import About from './About'
 import Skills from './Skills'
 import Projects from './Projects'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
 import useWindowSize from './hooks/useWindowSize'
+
+const AboutPage = lazy(() => import('./About'))
 
 function App() {
   const [sideBar, setSideBar] = useState(false)
@@ -23,11 +25,14 @@ function App() {
       ) : (
         <div style={{ color: 'red' }}></div>
       )}
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Home />
+        {/* Render the lazy-loaded component */}
+        <AboutPage />
+        <Skills />
+        <Projects />
+        <Footer />
+      </Suspense>
     </main>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const Home = () => {
   return (
     <section id="home" className="home">
