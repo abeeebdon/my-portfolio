62ad6791485a8ede2137243e293dccd7eb1ba7c6
Abeeb Maroof <abeebdon@gmail.com>
2025-04-18 08:03:42 +0100

    emaila

--- src/About.js
+++ src/About.js
@@ -8,26 +8,35 @@ const About = () => {
         <p className="max-w-[1440px] px-4 text-4xl font-bold mx-auto ">
           About <span className="text-red-600">Me</span>
         </p>
-        <p style={{ fontSize: "18px" }}>
+        <p style={{ fontSize: "20px" }}>
           I am Abeeb Maroof, a motivated and dedicated Frontend Developer with a
           passion for crafting intuitive and visually appealing user interfaces
           for both web applications and mobile devices. <br />
           With over three years of experience, I specialize in building
           efficient web applications and mobile applications using React.js,
-          React Native and Next.js. <br /> I have previously worked at{" "}
-          <span className="text-red-500">Entacrest Nexus </span>, where I worked
-          in a team of developers and designers to develop an ERP application,
-          event landing pages and organisation dashboard.
-          <br />I have also had the priviledge of being an HNG finalist. During
-          my time at <span className="text-red-500">HNG</span> , I collaborated
-          with a multidisciplinary team on real-world projects, including{" "}
-          <span className="text-red-500">Tifi</span>. This experience
-          strengthened my skills in frontend development and teamwork in an
-          agile environment. <br />I have volunteered and collaborated with
-          diverse institutions and developers. I enjoy taking on new challenges
-          and collaborating with clients to bring ideas to life through high
-          quality user interface.
+          React Native and Next.js. <br /> I have previously worked at various
+          organizations where I build scalable and optimized web applications
+          with great user interface and user experience. <br />I have
+          volunteered and collaborated with diverse institutions and developers.
+          I enjoy taking on new challenges and collaborating with clients to
+          bring ideas to life through high quality user interface.
         </p>
+        <div className="github-link">
+          <a
+            href="https://github.com/abeeebdon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GITHUB
+          </a>
+          <a
+            target="_blank"
+            href="https://docs.google.com/document/d/13JHMJn6lYmiokmuwLlU12pZQT1rPRrdtH9HT_n7w6c8/edit?usp=drivesdk"
+            rel="noreferrer"
+          >
+            MY CV
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,34 @@ const Home = () => {
   return (
     <section id="home" className="home">
       <div className="home-content">
-        <h3>Hello, it's Me</h3>
-        <h1>Abeeb Maroof</h1>
-        <h3>
-          I am a <span>Frontend Developer</span>
-        </h3>
+        <p>Hi 👋</p>
         <p>
-          I am highly experienced in creating responsive web pages with great
-          user interface and user experience
+          My name is <span className="text-5xl font-bold">Abeeb Maroof</span>
         </p>
-        <div className="github-link">
-          <a
-            href="https://github.com/abeeebdon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GITHUB
-          </a>
-          <a
-            target="_blank"
-            href="https://docs.google.com/document/d/13JHMJn6lYmiokmuwLlU12pZQT1rPRrdtH9HT_n7w6c8/edit?usp=drivesdk"
-            rel="noreferrer"
-          >
-            MY CV
-          </a>
-        </div>
+        <p>
+          Looking for a frontend developer who can transform your vision into
+          reality? I specialize in creating stunning, dynamic and highly
+          responsive websites that not only look beautiful but also drive
+          results.
+        </p>
+        <p>
+          With expertise in modern web technologies, I create fast-loading,
+          SEO-optimized sites that provide exceptional user experiences. From
+          pixel-perfect designs to smooth interactions, I'll help take your
+          online presence to the next level. Let's work together to build a
+          website that converts visitors into customers and sets your business
+          apart.
+        </p>
+        <p>
+          If you need a perfect hand on your software solutions, Kindly contact
+          me by clicking the button below.
+        </p>
+        <button
+          onClick={() => (window.location.href = "mailto:abeebdon@gmail.com")}
+          className="text-2xl font-bold bg-red-600 shadow-lg hover:bg-red-700 hover:shadow-xl transition-all duration-300 text-white px-10 py-3 rounded-lg"
+        >
+          Contact Me
+        </button>
       </div>
       <div className="image">
         <img src="./pics-removebg-preview.png" alt="my_pics" className="img" />
